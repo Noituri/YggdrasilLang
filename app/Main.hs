81c10a5e692c54a2b7654397df2543cd64c1e551
Mikolaj Radkowski <macbooc48@gmail.com>
2019-12-01 20:37:52 +0100

    Added some print lines

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,12 @@ import Lexer
 import Parser
 import Text.Megaparsec
 import Data.Text (pack)
+import Paths_YggdrasilLang (version)
+import Data.Version (showVersion)
 
 main :: IO ()
 main = do
+    putStrLn $ "Yggdrasil - v" ++ showVersion version
     file <- readFile "./app/tests/test1.yg"
+    putStrLn "Output of the parser:"
     parseTest (parseTopLevel <* eof) $ pack file
